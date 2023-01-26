64d0efbf545fc96a5be0653fece080a9ddf9a1f0
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-01-26 13:00:17 +0000

    fix smth

--- scripts/build-arturo.sh
+++ scripts/build-arturo.sh
@@ -11,23 +11,13 @@ sudo apt-get install libgmp-dev libmpfr-dev libgtk-3-dev libwebkit2gtk-4.0-dev
 curl -Lo- https://nim-lang.org/download/nim-1.6.10.tar.xz | tar Jxv
 
 if cd nim-1.6.10; then
-    cat > m <<< "
-all:
     ./build.sh
-    ./install.sh
-
-.PHONY: all"
-    make -f m -j $(nproc)
-    cd $HOME
+    ./install.sh || sudo ./install.sh
+    cd
 fi
 
 if cd arturo; then
-    cat > makefile <<< "
-all:
     ./build.nims install
-
-.PHONY: all"
-    make -j $(nproc)
 fi
 
 echo "BUILD_RESULT=$HOME/.arturo/bin" >> $GITHUB_ENV
