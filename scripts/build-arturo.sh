9ad21bbc701283cabab868e571ce4eb26f11cad0
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-01-26 12:38:45 +0000

    hmm

---
+++ scripts/build-arturo.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# a script for building Arturo (latest/nightly)
+
+checkout="${1:-master}"
+
+git clone -b "$checkout" https://github.com/arturo-lang/arturo
+
+sudo apt install libgmp-dev libmpfr-dev libgtk-3-dev libwebkit2gtk-4.0-dev
+
+if cd arturo; then
+    command -v make >/dev/null && {
+        cat > makefile << EOF
+.PHONY: all
+all:
+    ./build.nims install
+EOF
+        make -j8
+    } || ./build.nims install
+fi
+
+echo "BUILD_RESULT=$HOME/.arturo/bin" >> $GITHUB_ENV
