10bdd7473638b711e06a2f49bab1f4f5be1aab32
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-01-26 12:56:09 +0000

    hhh

--- scripts/build-arturo.sh
+++ scripts/build-arturo.sh
@@ -6,17 +6,28 @@ checkout="${1:-master}"
 
 git clone -b "$checkout" https://github.com/arturo-lang/arturo
 
-sudo apt install libgmp-dev libmpfr-dev libgtk-3-dev libwebkit2gtk-4.0-dev  nim make
+sudo apt-get install libgmp-dev libmpfr-dev libgtk-3-dev libwebkit2gtk-4.0-dev 
+
+curl -Lo- https://nim-lang.org/download/nim-1.6.10.tar.xz | tar Jxv
+
+if cd nim-1.6.10; then
+    cat > m <<< "
+all:
+    ./build.sh
+    ./install.sh
+
+.PHONY: all"
+    make -f m -j $(nproc)
+    cd $HOME
+fi
 
 if cd arturo; then
-    command -v make >/dev/null && {
-        cat > makefile << EOF
-.PHONY: all
+    cat > makefile <<< "
 all:
     ./build.nims install
-EOF
-        make -j8
-    } || ./build.nims install
+
+.PHONY: all"
+    make -j $(nproc)
 fi
 
 echo "BUILD_RESULT=$HOME/.arturo/bin" >> $GITHUB_ENV
