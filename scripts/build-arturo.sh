4a2e3b470a5f24bda786e48baf499722f1ae4a41
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-01-27 14:18:58 +0000

    update

--- scripts/build-arturo.sh
+++ scripts/build-arturo.sh
@@ -3,24 +3,14 @@
 # a script for building Arturo (latest/nightly)
 
 checkout="${1:-master}"
-
-git clone -b "$checkout" https://github.com/arturo-lang/arturo
  
 curl -Lo- https://nim-lang.org/download/nim-1.6.10.tar.xz | tar Jxv
 
-sudo apt-get install \
-    libgtk-3-dev libwebkit2gtk-4.0-dev  \
-    libmpfr-dev libgmp-dev  \
-    -qy
-
-###
-
 fast_cmd() {
     tee m <<< "
+.PHONY: all
 all:
-    $*
-
-.PHONY: all"
+    $*"
     if ! make -j $(nproc) -f m; then
         return 1
     fi
@@ -32,6 +22,11 @@ if cd nim-1.6.10; then
     sudo ./install.sh /usr/bin || ./install.sh /usr/bin
     cd
 fi
+
+sudo apt-get install \
+    libgtk-3-dev libwebkit2gtk-4.0-dev  \
+    libmpfr-dev libgmp-dev  \
+    -qy
 
 git clone -b "$checkout" https://github.com/arturo-lang/arturo
 
