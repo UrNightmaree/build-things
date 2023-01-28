6c8c8076f3221c5d63475aa80263749b188ea377
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-01-28 10:26:40 +0000

    fix

--- scripts/build-helix.sh
+++ scripts/build-helix.sh
@@ -7,21 +7,19 @@ checkout="${1:-master}"
 . scripts/util-cmd.sh
 
 
-#curl -sSf https://sh.rustup.rs  \
-#    > rustup.sh
-#sh rustup.sh -y
-#
-## shellcheck source=/dev/null
-#source "$HOME/.cargo/env"
+curl -sSf https://sh.rustup.rs  \
+    > rustup.sh
+sh rustup.sh -y
 
-sudoo apt-get install rustc cargo -qy
+# shellcheck source=/dev/null
+source "$HOME/.cargo/env"
 
 git clone https://github.com/helix-editor/helix
 
 if cd helix; then
     git checkout "$checkout"
 
-    cargo install --locked --path helix-term || exit 1
+    cargo install --path helix-term || exit 1
     cp "$HOME/.cargo/bin/hx" /artifacts
     cp -r runtime /artifacts
 fi
