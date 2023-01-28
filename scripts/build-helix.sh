be733315a2d43126a59974534be9b122d7e9cec3
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-01-28 03:31:59 +0000

    fix cargo

--- scripts/build-helix.sh
+++ scripts/build-helix.sh
@@ -19,6 +19,11 @@ git clone https://github.com/helix-editor/helix
 
 if cd helix; then
     git checkout "$checkout"
+
+    # https://github.com/rust-lang/cargo/issues/6513#issuecomment-950252753
+    git config --global --unset http.proxy  
+    git config --global --unset https.proxy
+
     cargo install --locked --path helix-term
     cp "$HOME/.cargo/bin/hx" /artifacts
     cp -r runtime /artifacts
