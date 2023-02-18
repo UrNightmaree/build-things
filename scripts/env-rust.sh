1dfb6f473460721011621df0cbb8157e1d312229
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-02-18 01:13:13 +0000

    fix

--- scripts/env-rust.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# a script for setting up rust environment
-
-export CARGO_HOME=/opt/rust/cargo
-export RUSTUP_HOME=/opt/rust/rustup
-
-export PATH="$CARGO_HOME/bin:$PATH"
--- scripts/install-deps-helix.sh
+++ scripts/install-deps-helix.sh
@@ -2,6 +2,6 @@
 
 # a script for installing helix deps
 
-. env-rust.sh
+curl -sSf https://sh.rustup.rs | bash -s -- -y
 
-curl -sSf https://sh.rustup.rs | bash -s -- -y --no-modify-path
+source "$HOME/.cargo/env"
