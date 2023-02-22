a2a96f110a3a016b9e4d4a16e3f1ae1dc15c81bf
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-02-22 12:41:27 +0000

    changes

--- scripts/build-sumneko-lua.sh
+++ scripts/build-sumneko-lua.sh
@@ -11,10 +11,7 @@ git clone --depth=1 https://github.com/sumneko/lua-language-server
 if cd lua-language-server; then
     git checkout "$checkout"
     git submodule update --depth 1 --init --recursive
-    cd 3rd/luamake || exit
-    ./compile/install.sh
-    cd ../.. || exit
-    ./3rd/luamake/luamake rebuild
+    ./make.sh || exit 1
     cp -r bin doc log meta tools locale script "${ARTIFACTS:=/artifacts}"
     cp debugger.lua main.lua "$ARTIFACTS"
 fi
